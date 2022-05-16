28839278e24037f3c56011e9c8a68df876d4cfa4
Bryan Anderson <bryan.anderson@cfwebtools.com>
2022-05-16 17:50:35 -0400

    Added express validation

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 
 const router = express.Router();
 
@@ -6,8 +7,27 @@ router.get('/', (req, res) => {
     res.render('form', { title: 'Registration Form' });
 });
 
-router.post('/', (req, res) => {
-    res.render('form', { title: 'Registration Form' });
-});
+router.post('/', 
+    [
+        body('name')
+            .isLength({ min: 3 })
+            .withMessage('Please enter a name.'),
+        body('email')
+            .isLength({ min: 3 })
+            .withMessage('Please enter an email.'),
+    ],(req, res) => {
+        const errors = validationResult(req);
+
+        if (errors.isEmpty()) {
+            res.send('Thank you for your registration!');
+        } else {
+            res.render('form', { 
+                title: 'Registration Form',
+                errors: errors.array(),
+                data: req.body,
+            });
+        }
+    }
+);
 
 module.exports = router;
