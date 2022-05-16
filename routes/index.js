0f1c65ece9c2bb97e271b57c9bc4128e41fd25f1
Bryan Anderson <bryan.anderson@cfwebtools.com>
2022-05-16 16:29:00 -0400

    added layout and updated form view

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('form');
+    res.render('form', { title: 'Registration Form' });
+});
+
+router.post('/', (req, res) => {
+    res.render('form', { title: 'Registration Form' });
 });
 
 module.exports = router;
